e05a6b009cf24846b016f3ffecb551b91d9684e4
DTripleD <DTripleD@users.noreply.github.com>
2024-01-04 19:49:14 +0000

    deploy: b3cc16b91fcfa2c0fb43a59dad3d177f01087b6f

---
+++ assets/ContactsPage-FhHnMlT7.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-yDIE8QJK.js";const a=()=>t.jsx("div",{children:"Contacts Page"});export{a as default};
---
+++ assets/ErrorPage-y1F_Ljca.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-yDIE8QJK.js";const o=()=>r.jsx("div",{children:"Error Page"});export{o as default};
